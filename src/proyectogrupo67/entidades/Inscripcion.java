f1ad965174776fece6c3357b340d8a1f63a129f7
Gabriel Cuervo <gabicuervo2001@hotmail.com>
2023-09-11 11:03:11 -0300

    traspaso de archivos

---
+++ src/proyectogrupo67/entidades/Inscripcion.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectogrupo67.entidades;
+
+import proyectogrupo67.entidades.Alumno;
+import proyectogrupo67.entidades.Materia;
+/**
+ *
+ * @author julian
+ */
+public class Inscripcion {
+    
+    private int idInscripcion;
+    private Alumno alumno;
+    private Materia materia;
+    private Double nota;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(int idInscripcion, Alumno alumno, Materia materia, Double nota) {
+        this.idInscripcion = idInscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripcion(Alumno alumno, Materia materia, Double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public int getIdInscripcion() {
+        return idInscripcion;
+    }
+
+    public void setIdInscripcion(int idInscripcion) {
+        this.idInscripcion = idInscripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+    
+    
+    
+}
---
+++ src/proyectogrupo67/entidades/Materia.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectogrupo67.entidades;
+
+/**
+ *
+ * @author julian
+ */
+public class Materia {
+    
+    private int idMateria;
+    private String nombre;
+    private int añoMateria;
+    private Boolean activo;
+
+    public Materia() {
+        
+    }
+
+    public Materia(int idMateria, String nombre, int añoMateria, Boolean activo) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.añoMateria = añoMateria;
+        this.activo = activo;
+    }
+
+    public Materia(String nombre, int añoMateria, Boolean activo) {
+        this.nombre = nombre;
+        this.añoMateria = añoMateria;
+        this.activo = activo;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAñoMateria() {
+        return añoMateria;
+    }
+
+    public void setAñoMateria(int añoMateria) {
+        this.añoMateria = añoMateria;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", a\u00f1oMateria=" + añoMateria + ", activo=" + activo + '}';
+    }
+    
+    
+}
