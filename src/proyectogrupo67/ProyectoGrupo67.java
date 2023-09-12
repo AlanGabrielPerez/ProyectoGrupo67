a08d8e0f64e538ba36d967a056bd0d55803a1b21
Gabriel Cuervo <gabicuervo2001@hotmail.com>
2023-09-12 12:30:46 -0300

    update

--- src/proyectogrupo67/ProyectoGrupo67.java
+++ src/proyectogrupo67/ProyectoGrupo67.java
@@ -21,14 +21,11 @@ public class ProyectoGrupo67 {
     public static void main(String[] args) {
         Alumno alu = new Alumno ("NetBeans", "F", LocalDate.of(1898, 03, 20), true, 23435345);
         AlumnoData enviar = new AlumnoData();
-        enviar.guardarAlumno(alu);
+        //enviar.guardarAlumno(alu);
         //enviar.modificarAlumno(alu);
-<<<<<<< HEAD
-        
-=======
         Alumno alum = enviar.buscarAlumnoDni(111233);
         System.out.println(alum.getApellido());
->>>>>>> 70256cb14cee68d91ae98c61bdab802ee54f65b5
+
         
     }
     
