7a49cee3fd4f1a5c561b9e9a0037f7f96797ddd1
MoonshineJules <moonshineshules@gmail.com>
2023-09-13 10:24:36 -0300

    ignorar

--- src/proyectogrupo67/ProyectoGrupo67.java
+++ src/proyectogrupo67/ProyectoGrupo67.java
@@ -23,7 +23,7 @@ public class ProyectoGrupo67 {
         AlumnoData enviar = new AlumnoData();
         //enviar.guardarAlumno(alu);
         //enviar.modificarAlumno(alu);
-        Alumno alum = enviar.buscarAlumnoDni(111233);
+        Alumno alum = enviar.buscarAlumnoDni(13435245);
         System.out.println(alum.getApellido());
 
         
